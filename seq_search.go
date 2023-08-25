729dec5dcb920fd05f23f223b9fd5ac89d64d927
Nicholas Moser <NicholasMoser56@gmail.com>
2023-08-24 21:07:31 -0500

    Endlessly query new hex bytes

--- seq_search.go
+++ seq_search.go
@@ -14,27 +14,13 @@ import (
 )
 
 func main() {
-	version := "1.0.0"
+	version := "1.1.0"
 	fmt.Printf("SEQ Search %s\n", version)
-
-	// Get user input
-	fmt.Print("Bytes in hex: ")
-	var hexBytes string
-	in := bufio.NewReader(os.Stdin)
-	hexBytes, err := in.ReadString('\n')
-	hexBytes = strings.Replace(hexBytes, "0x", "", -1)
-	hexBytes = strings.Replace(hexBytes, " ", "", -1)
-	hexBytes = strings.TrimSpace(hexBytes)
-	bytePattern, err := hex.DecodeString(hexBytes)
-	if err != nil {
-		fmt.Println("Invalid hex bytes")
-		exit(1)
-	}
 
 	fmt.Print("SEQ files directory: ")
 	var dir string
-	in = bufio.NewReader(os.Stdin)
-	dir, err = in.ReadString('\n')
+	in := bufio.NewReader(os.Stdin)
+	dir, err := in.ReadString('\n')
 	dir = strings.TrimSpace(dir)
 	fileInfo, err := os.Stat(dir)
 	if err != nil {
@@ -47,17 +33,33 @@ func main() {
 	}
 	fmt.Println()
 
-	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
-		if !d.IsDir() && strings.HasSuffix(path, ".seq") {
-			searchFile(path, bytePattern)
+	for {
+		// Get user input
+		fmt.Print("Bytes in hex: ")
+		var hexBytes string
+		in := bufio.NewReader(os.Stdin)
+		hexBytes, err := in.ReadString('\n')
+		hexBytes = strings.Replace(hexBytes, "0x", "", -1)
+		hexBytes = strings.Replace(hexBytes, " ", "", -1)
+		hexBytes = strings.TrimSpace(hexBytes)
+		bytePattern, err := hex.DecodeString(hexBytes)
+		if err != nil {
+			fmt.Println("Invalid hex bytes")
+			exit(1)
 		}
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("Impossible to walk directories: %s", err)
-		exit(1)
+		fmt.Println()
+
+		err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+			if !d.IsDir() && strings.HasSuffix(path, ".seq") {
+				searchFile(path, bytePattern)
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatalf("Impossible to walk directories: %s", err)
+			exit(1)
+		}
 	}
-	exit(0)
 }
 
 func searchFile(path string, bytePattern []byte) {
@@ -68,7 +70,7 @@ func searchFile(path string, bytePattern []byte) {
 	index := suffixarray.New(dat)
 	offsets := index.Lookup(bytePattern, -1)
 	if len(offsets) > 0 {
-		fmt.Printf("%s at offsets: ", path)
+		fmt.Printf("%s at hex offsets: ", path)
 		sort.Ints(offsets)
 		for i := 0; i < len(offsets); i++ {
 			fmt.Printf("%X ", offsets[i])
